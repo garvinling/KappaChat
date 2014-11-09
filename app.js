bea4acfdf206f56e8c7c69946f3ff09880568990
GarvinLing <gling8313@gmail.com>
2014-11-08 20:13:20 -0800

    added live updating usernames

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var io = require('socket.io')(http);
 var people = {};
 var fs = require('fs');
 //var server = require('http').createServer(app);
+var users = [];
+var user_string = "";
 var port = process.env.PORT || 3000;
 
 	app.use("/img", express.static(__dirname + '/img'));
@@ -21,23 +23,31 @@ io.on('connection', function(socket){
 
 	socket.on("join",function(name){
 
-		people[socket.id] = name;
-		console.log(people[socket.id] + " has joined the chat.");
-		//io.emit("update", people[socket.id] ," has joined the chat."); //Broadcast join message across all sockets. 
-
+		if(users.indexOf(name) == -1)
+		{
+			people[socket.id] = name;
+			users[socket.id] = name;
+			console.log(people[socket.id] + " has joined the chat.");
+			user_string = "";
+			io.emit('addUser',people);
+		}
 	});
 	
 	socket.on('chat message' , function(msg){
 		
 		console.log('message: ' + msg);
-
 		io.emit('chat message', people[socket.id], msg);
 
 	});
 
+
 	socket.on('disconnect',function(){	
 		
-		console.log('User Has Disconnected: ');
+		console.log('User Has Disconnected.');
+		printUsers();
+
+		delete people[socket.id];
+		delete users[socket.id];
 
 		if(people[socket.id] != "undefined")
 		{
@@ -56,7 +66,18 @@ io.on('connection', function(socket){
 });
 
 
+function printUsers()
+{
 
+	for(var index in people)
+	{
+
+		user_string = user_string + "<li>" + people[index] + "</li>";
+	
+	}
+	
+	io.emit('addUser',user_string);
+}
 
 http.listen(port, function() {
 
@@ -65,7 +86,11 @@ http.listen(port, function() {
  });
 
 
+function log(str){
 
+	console.log(str);
+
+}
 // // Development 
 // http.listen(3000,function(){
 
